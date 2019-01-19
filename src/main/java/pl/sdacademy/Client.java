ca980e13592434f02e29f981e351a9ae41e05a55
Grzegorz <grzegorz.wodo@gmail.com>
2019-01-19 10:56:48 +0100

    .

--- src/main/java/pl/sdacademy/Client.java
+++ src/main/java/pl/sdacademy/Client.java
@@ -3,6 +3,7 @@ package pl.sdacademy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,12 +11,43 @@ public class Client {
     public static void main(String[] args) throws IOException {
 
         System.out.println("Enter the IP address of a machine running the date server:");
+
         String serverAddress = new Scanner(System.in).nextLine();
         Socket socket = new Socket(serverAddress, 9090);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String response = in.readLine();
-        System.out.println("Server response: " + response);
 
+        //wyslij do server podane liczby "5 5 6 128"
+        System.out.println("Podaj liczby: ");
+        String numbers = new Scanner(System.in).nextLine();
+        out.println(numbers);
+
+        //wyswietl otrzymynay wynik
+        System.out.println(in.readLine());
+
+
+
+
+
+
+
+
+
+
+
+
+        /* //Browser
+        Socket socket = new Socket("sejm.gov.pl", 80);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("GET / HTTP/1.1");
+        out.println("Host: sejm.gov.pl");
+        out.println();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line = "";
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        */
     }
 
 }
--- src/main/java/pl/sdacademy/Server.java
+++ src/main/java/pl/sdacademy/Server.java
@@ -1,6 +1,8 @@
 package pl.sdacademy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,8 +14,27 @@ public class Server
         ServerSocket listener = new ServerSocket(9090);
         System.out.println("Date server is running");
         Socket socket = listener.accept();
+
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        out.println(new Date().toString());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //odczytaj od klienta ciag liczb ( 5 6 7 8 9)
+        String numbers = in.readLine();
+        System.out.println(numbers);
+
+        //"5 6 7 8".split(" ") podzel stringa na liczby
+        //i zsumuj je
+        String[] tab = numbers.split(" ");
+        int sum = 0;
+        for (String s : tab) {
+            int val = Integer.parseInt(s);
+            sum +=val;
+        }
+
+        //wyslij wynik do klienta
+        out.println(sum);
+
+        //out.println(new Date().toString());
 
     }
 }
