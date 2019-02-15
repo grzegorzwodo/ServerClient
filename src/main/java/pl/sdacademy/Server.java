faf51dde21b46e31d74abb5e710a39ec065d989f
Grzegorz <grzegorz.wodo@gmail.com>
2019-02-15 20:06:15 +0100

    .

--- src/main/java/pl/sdacademy/Server.java
+++ src/main/java/pl/sdacademy/Server.java
@@ -1,8 +1,6 @@
 package pl.sdacademy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,30 +9,12 @@ import java.util.Date;
 public class Server
 {
     public static void main( String[] args ) throws IOException {
-        ServerSocket listener = new ServerSocket(9090);
+
+        ServerSocket listener = new ServerSocket(666);
         System.out.println("Date server is running");
         Socket socket = listener.accept();
-
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        //odczytaj od klienta ciag liczb ( 5 6 7 8 9)
-        String numbers = in.readLine();
-        System.out.println(numbers);
-
-        //"5 6 7 8".split(" ") podzel stringa na liczby
-        //i zsumuj je
-        String[] tab = numbers.split(" ");
-        int sum = 0;
-        for (String s : tab) {
-            int val = Integer.parseInt(s);
-            sum +=val;
-        }
-
-        //wyslij wynik do klienta
-        out.println(sum);
-
-        //out.println(new Date().toString());
+        out.println(new Date().toString());
 
     }
 }
